444435a817c5dcb935f9c7b3c3b575f6636d0f91
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-11-24 11:41:01 -0300

    commit 2

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,10 @@
 import { Negociacao } from "./models/negociacao.js";
+import { NegaciacaoController } from "./controllers/negaciacao-controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100);
-console.log(negociacao);
+const controller = new NegaciacaoController();
+const form = document.querySelector('.form');
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  controller.adiciona();
+});
+
